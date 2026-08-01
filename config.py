01c76d756399ab403945178e824d1ab43fd5d14a
Yotam tom Goren <yotamgo@mta.ac.il>
2026-08-01 13:10:40 +0300

    crating the main fils

---
+++ config.py
@@ -0,0 +1,10 @@
+#colors settings:
+BLACK = (0, 0, 0)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+
+#screen settings:
+WINDOW_WIDTH = 600
+WINDOW_HEIGHT = 600
+CELL_SIZE = 20  #set the pixel size
--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-1
+import pygame #imports
+import config
+import game_objects
+pygame.init()  #starting the pygame Tools
+screen=pygame.display.set_mode() #creates the monitor
+running=True
+
+while running: #the main loop of the game
+    for event in pygame.event.get(): #check for every event possible
+        if event.type == pygame.QUIT:
+            running = False # if the event is QUIT End the loop
+
+
+
+
+    pygame.display.update() #update the screen for each event
