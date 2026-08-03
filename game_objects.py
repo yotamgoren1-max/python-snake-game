cfe583238a7d82f911c2857a2a34729e30e0cc82
Yotam tom Goren <yotamgo@mta.ac.il>
2026-08-03 12:17:15 +0300

    adding the Snake class

--- game_objects.py
+++ game_objects.py
@@ -31,6 +31,61 @@ class Apple:
     def draw_apple(self,surface):
         surface.blit(self.surface, self.rect)
 
+class Snake:
+
+    def __init__(self):
+        self.x = (config.WINDOW_WIDTH // 2 // config.CELL_SIZE) * config.CELL_SIZE  # middle of the screen
+        self.y = (config.WINDOW_HEIGHT // 2 // config.CELL_SIZE) * config.CELL_SIZE
+        self.body=[]
+        head_po=pygame.Rect((self.x, self.y, config.CELL_SIZE, config.CELL_SIZE))
+        body_po=pygame.Rect((self.x-config.CELL_SIZE, self.y, config.CELL_SIZE, config.CELL_SIZE))
+        self.body.append(head_po)
+        self.body.append(body_po)
+        self.surface = pygame.Surface((config.CELL_SIZE, config.CELL_SIZE))  # the surface of the head and fill it with green
+        self.surface.fill(config.GREEN)
+        self.direction = config.RIGHT
+
+    def draw(self,surface):
+        for i in range (0,len(self.body)):
+            surface.blit(self.surface, self.body[i])
+
+    def move (self):
+        new_x = self.body[0].x + self.direction[0]
+        new_y = self.body[0].y + self.direction[1]
+        new_head=pygame.Rect((new_x, new_y, config.CELL_SIZE, config.CELL_SIZE))
+        self.body.insert(0,new_head)
+        self.body.pop()
+
+    def grow (self):
+        dir_x = self.body[-1].x - self.body[-2].x
+        dir_y = self.body[-1].y - self.body[-2].y
+        new_x = self.body[-1].x + dir_x
+        new_y = self.body[-1].y + dir_y
+        new_tail=pygame.Rect((new_x, new_y, config.CELL_SIZE, config.CELL_SIZE))
+        self.body.append(new_tail)
+
+    def check_apple_collision(self,apple):
+        return self.body[0].colliderect(apple.rect)
+
+
+    def check_wall_collision(self):
+        flag=False
+        head=self.body[0]
+        if head.x < 0 or head.x >= config.WINDOW_WIDTH or head.y < 0 or head.y >= config.WINDOW_HEIGHT:
+            flag=True
+        return flag
+
+    def check_self_collision(self):
+        head = self.body[0]
+        for i in range(1,len(self.body)):
+            if head.x== self.body[i].x and head.y== self.body[i].y:
+                return True
+        return False
+
+
+
+
+
 
 
 
